dd260109b50183e8cdefa968b0ccc4f44ed98775
Till Brede <tb@symcon.de>
2019-09-26 16:14:10 +0200

    - added test submodule - added validation test

---
+++ tests/AlarmierungValidationTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+include_once __DIR__ . '/stubs/Validator.php';
+class AlarmierungValidationTest extends TestCaseSymconValidation
+{
+    public function testValidateAlarmierung(): void
+    {
+        $this->validateLibrary(__DIR__ . '/..');
+    }
+    public function testValidateAlarmierungModule(): void
+    {
+        $this->validateModule(__DIR__ . '/../Alarmierung');
+    }
+}
