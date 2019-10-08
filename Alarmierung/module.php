505efdb93d7fe32d92fceec63fee47179974812d
Till Brede <tb@symcon.de>
2019-10-08 12:16:19 +0200

    Removed unnecessary constant definition

--- Alarmierung/module.php
+++ Alarmierung/module.php
@@ -1,12 +1,6 @@
 <?php
 
 declare(strict_types=1);
-if (!defined('IPS_BASE')) {
-    define('IPS_BASE', 10000);
-}
-if (!defined('VM_UPDATE')) {
-    define('VM_UPDATE', IPS_BASE + 603);
-}
 
     class Alarmierung extends IPSModule
     {
